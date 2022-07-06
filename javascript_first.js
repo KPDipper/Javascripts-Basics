c6109ad88cc256cfb5d78cdf0ce80ff1ebd9aae2
KPDipper <pokhare2468@gmail.com>
2022-07-06 11:06:02 +0545

    Javascript for teaching purpose

---
+++ javascript_first.js
@@ -0,0 +1,33 @@
+
+
+//output method
+//generaly testing ko lagi use garchyoum not generally
+
+function hello(){
+    document.write("Hello world")
+
+}
+
+
+document.getElementById('heading1').innerHTML="JAVASCRIPT"
+
+//form haru reload garda aucha ni like f we submitted the form
+// window.alert('This is alert message')
+
+// //print ko dilaog matra open huncha
+// window.print()
+
+//these are all output
+
+//now for inputs
+//here maile j value prompt ma ayo tei value abha prompt gayera bascha
+// a=prompt("Enter anything")
+//now to print value in a
+// document.write(a)
+
+a=document.getElementById('_name').value
+console.log(a)
+document.write(a)
+
+
+//now lets take inpout from form
